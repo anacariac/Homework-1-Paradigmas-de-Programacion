50224b19a1a677ebb1340368021aefb87c6aa47e
anacariac <acabreracariac@udesa.edu.ar>
2025-03-13 14:34:03 -0300

    ejercicio 4.2

--- ej4/ejercicio4.cpp
+++ ej4/ejercicio4.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 bool compare(const char* s1, const char* s2){
     if(*s1 == '\0' && *s2 == '\0'){
@@ -11,7 +14,21 @@ bool compare(const char* s1, const char* s2){
     return compare(s1 + 1, s2 + 1);
 }
 
+
 int main(){
-    cout<< compare("hola", "hoLa")<<endl;
+    string s1,s2;
+    cout << "Ingrese la primer palabra a comparar: "<< endl;
+    cin >> s1;
+    cout << "Ingrese la segunda palabra a comparar: "<<endl;
+    cin >> s2;
+
+    auto startTime = high_resolution_clock::now();
+
+    cout<< compare(s1.c_str(), s2.c_str())<<endl;
+
+    auto endTime = high_resolution_clock::now();
+    auto elapsedTime = duration_cast<nanoseconds>(endTime - startTime);
+
+    cout<<"A compare() le tomo: "<< elapsedTime.count() << " nanosegundos"<< endl;
     return 0;
 }
