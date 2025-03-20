d5d6c75a94c55b59bec899108c3fa479fd42a1a6
anacariac <acabreracariac@udesa.edu.ar>
2025-03-20 08:31:29 -0300

    Actualización en ejercicio2.hpp

--- ej2/ejercicio2.hpp
+++ ej2/ejercicio2.hpp
@@ -2,8 +2,18 @@
 using namespace std;
 enum class Level { DEBUG, INFO, WARNING, ERROR, CRITICAL};
 
+/*
+ Convierte un nivel de severidad a su representación en texto.
+ severity<enum class> Nivel de severidad (DEBUG, INFO, WARNING, ERROR, CRITICAL).
+ retorna string la representación en texto del nivel de severidad.
+ */
 string convertLevel(Level severity);
 
+/*
+ Registra un mensaje en un archivo de log con un nivel de severidad.
+ message<string> Mensaje a registrar.
+ severity<enum class> Nivel de severidad del mensaje.
+ */
 void logMessage(const string message, Level severity);
 
 void logMessage(const string message, const string file, int line);
